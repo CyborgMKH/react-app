bdf5c3c4c63ddc6d576ead32fa927dcd9082b3a1
cyborgmkh <sumanmukhiya@ismt.edu.np>
2023-05-15 18:09:41 +0545

    make changes in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Product from "./Product";
+import About from "./About";
 
 const Home = () => {
   const arr = [1, 2, 3, 4];
   return (
-    <div>
-      {arr.map((i) => (
-        <Product value={i} key={i} />
-      ))}
-    </div>
+    <>
+      <div>
+        <About />
+        {arr.map((i) => (
+          <Product value={i} key={i} />
+        ))}
+      </div>
+      <div>
+        <p>This is a paragraph hai guys.....!</p>
+      </div>
+    </>
   );
 };
 
